faab4c74d4b3aefd761acd8073c4c70a6d69c8ee
Mathew Kostrzewa <mathew.kostrzewa@dcmail.ca>
2018-03-21 09:22:21 -0400

    javascript login test

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "admin" && password == "admin"){
+alert ("Login successfully");
+window.location = "classwork3.html"; // Redirecting to other page.
+return false;
+}
+}
